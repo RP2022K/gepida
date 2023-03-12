8321bd5c18f53a6f04ac0b440529ff3b0c9c2aa8
Gyüre Árpád <arpadgyure5@gmail.com>
2023-03-12 20:17:05 +0100

    first work

---
+++ src/app.js
@@ -0,0 +1,39 @@
+
+const bicycleBody =document.querySelector("#bicycleBody");
+
+const gepidaArray = [
+    {name: "Cassis", wheel: 28, usage: "offroad", price: 557900},
+    {name: "Alboin 900", wheel: 28, usage: "trekking", price: 519900},
+    {name: "Asgard", wheel: 29, usage: "technikás utak", price: 519900},
+    {name: "Ruga", wheel: 29, usage: "hegyi", price: 372900},
+    {name: "Reptila", wheel: 28, usage: "városi", price: 308900},
+    {name: "Sirmium", wheel: 29, usage: "hegyi", price: 264900}
+]
+
+function loadTable(){
+    gepidaArray.forEach((bikeType) => {
+        let tr = document.createElement('tr');
+        let tdName = document.createElement('td');
+        let tdWheel = document.createElement('td');
+        let tdUsage = document.createElement('td');
+        let tdPrice = document.createElement('td');
+
+        tdName.textContent = bikeType.name;
+        bicycleBody.append(tr);
+        tr.append(tdName);
+
+        tdWheel.textContent = bikeType.wheel;
+        bicycleBody.append(tr);
+        tr.append(tdWheel);
+
+        tdUsage.textContent = bikeType.usage;
+        bicycleBody.append(tr);
+        tr.append(tdUsage);
+
+        tdPrice.textContent = bikeType.price;
+        bicycleBody.append(tr);
+        tr.append(tdPrice);
+    })
+}
+
+loadTable();
